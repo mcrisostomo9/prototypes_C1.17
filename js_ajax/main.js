4e735ceb263a3d715a5f46f1795e44f9ba7a3201
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-04 17:08:01 -0800

    js ajax - Mark Crisostomo UPDATED

--- js_ajax/main.js
+++ js_ajax/main.js
@@ -9,11 +9,11 @@ $(document).ready(function(){
             url: 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json',
             success: function(result) {
                 console.log('AJAX Success function called, with the following result:', result);
-                global_result = result;
-                for(i=0;i < global_result.feed.entry.length-1; i++){
-                    var movies_pic = global_result.feed.entry[i]['im:image'][2].label;
-                    var movie_title = global_result.feed.entry[i]['im:name'].label;
-                    var movie_director = global_result.feed.entry[i]['im:artist'].label;
+                global_result = result.feed.entry;
+                for(i=0;i < global_result.length-1; i++){
+                    var movies_pic = global_result[i]['im:image'][2].label;
+                    var movie_title = global_result[i]['im:name'].label;
+                    var movie_director = global_result[i]['im:artist'].label;
                     $('<img>').attr('src', movies_pic).appendTo('#main');
                     $('<h4>').text(movie_title).appendTo('#main');
                     $('<p>').text('Directed by: ' + movie_director).appendTo('#main');
