26b42de8eceddb32a041f9ef1efb4bf7bf4a007b
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-19 21:14:29 -0800

    initial webserver .1

--- node_webserver/webserver.js
+++ node_webserver/webserver.js
@@ -1,1 +1,12 @@
 
+var http = require('http');
+
+var server = http.createServer(function handleRequest(request, response){
+  console.log('handling request');
+  response.write('initialized response \n');
+  response.end('Respnse received ' + request.url);
+});
+
+server.listen(8888, function () {
+  console.log('server listening');
+});
