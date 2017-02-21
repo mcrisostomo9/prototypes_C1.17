3fdabf9e6a9c8bc8c362d1080db924f99ea4dabb
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-21 13:06:22 -0800

    done, need to pull

--- ang_services_intro/script.js
+++ ang_services_intro/script.js
@@ -3,8 +3,9 @@ var app = angular.module("markApp", []);
 app.controller('dataController', function($http, $log){
   var self = this;
   self.artistName;
+
   self.urlGenerator = function (artist) {
-      var searchArtist =  "https://itunes.apple.com/search?term=[" + [artist] + "]&callback=JSON_CALLBACK";
+      var searchArtist =  "https://itunes.apple.com/search?term=" + artist + "&callback=JSON_CALLBACK";
       $log.log(searchArtist);
       return searchArtist;
   }
@@ -13,13 +14,16 @@ app.controller('dataController', function($http, $log){
     var urlSearch = self.urlGenerator(search);
     $http({
       url: urlSearch,
-      method: 'post',
-      dataType: 'json',
-      cache: false
+      method: 'jsonp',
+      cache: false,
+      dataType: 'json'
+
     })
     .then(
       function(response){
-        $log.log(response.data)
+        $log.log('successful call', response);
+        self.artistData = response.data;
+        $log.log(self.artistData.artistName);
       },
       function(response){
         $log.error('error in call')
