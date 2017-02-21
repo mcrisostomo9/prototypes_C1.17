803f56ac61047c0662216254e2ecce2ce8dedfeb
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-20 21:43:18 -0800

    need to understand what json_callback is

---
+++ ang_services_intro/script.js
@@ -0,0 +1,31 @@
+var app = angular.module("markApp", []);
+
+app.controller('dataController', function($http, $log){
+  var self = this;
+  self.artistName;
+  self.urlGenerator = function (artist) {
+      var searchArtist =  "https://itunes.apple.com/search?term=[" + [artist] + "]&callback=JSON_CALLBACK";
+      return searchArtist;
+  }
+
+  self.getArtist = function(search){
+    var urlSearch = self.urlGenerator(search);
+    $http({
+      url: urlSearch,
+      method: 'post',
+      dataType: 'json',
+      cache: false
+    })
+    .then(
+      function(response){
+        $log.log(response.data)
+      },
+      function(response){
+        $log.log('error in call')
+      }
+    );
+  }
+
+
+
+});
