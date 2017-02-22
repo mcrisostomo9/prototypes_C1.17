a065a6207fe99dfd1d5018ea33cdf42bbfc2416f
Mark Crisostomo <mcrisostomo9@gmail.com>
2017-02-21 18:08:57 -0800

    fs1

--- mysql_basics/mysql_queries.sql
+++ mysql_basics/mysql_queries.sql
@@ -1,1 +1,7 @@
 Place your queries below
+SELECT * FROM `students` WHERE 'name' = 'Charles'
+SELECT * FROM `students` WHERE 'age' = '25'
+UPDATE `students` SET `email`='updatedemail@gmail.com' WHERE `username`='mrgnarly'
+INSERT INTO `students` SET `username`='Derrick', `email`='deeeze@email.com', `password`=sha1('abteojfksafsf')
+UPDATE `students` SET `name`='Derrick S', `ssn`='89098', `age`='29' WHERE `username`='Derrick'
+DELETE FROM `students` WHERE `username`='Derrick'
